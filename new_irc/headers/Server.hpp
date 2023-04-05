8b78c2910003e4de51f1eb706b56ad92b9e73e00
Deniz Karhan <81527587+denizkarhan@users.noreply.github.com>
2023-04-05 23:33:52 +0300

    Add files via upload

--- new_irc/headers/Server.hpp
+++ new_irc/headers/Server.hpp
@@ -1,24 +1,34 @@
-#ifndef SERVER_HPP
-# define SERVER_HPP
-
 #include "Utils.hpp"
 #include "Client.hpp"
 
-class Server
-{
+class Server{
     private:
-        int _socketFd;
-        int _port;
-        std::string _password;
+        struct sockaddr_in  _addr;
+        socklen_t           _addr_len;
+        int                 _server_fd;
+        int                 _port;
+        char*               _password;
+        char                _buffer[BUFFER_SIZE];
+        struct pollfd       _pollfds[USER_SIZE];
 
-        std::vector<Client *> _clients;
-        std::vector<Channel *>  _channels;
+        std::map<int, Client *> _clients;
 
     public:
         Server(char **av);
         ~Server();
-
-        int createSocket();
+        void                createSocket();
+        int                 getPort();
+        int                 getServerfd();
+        char*               getPassword();
+        socklen_t           getAddrlen();
+        struct sockaddr_in  getAddr();
+        
+        void                setPort(int port);
+        void                setServerfd(int server_fd);
+        void                setPassword(char* password);
+        void                setAddrlen(socklen_t addrLen);
+        void                setAddr(struct sockaddr_in addr);
+        
+        void                createSocket();
+        void                serverInfo(char *message);
 };
-
-#endif
