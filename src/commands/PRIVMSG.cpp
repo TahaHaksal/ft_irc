c8995cfcf5d5f2fe52b95ccc8988826aed258884
Deniz Karhan <81527587+denizkarhan@users.noreply.github.com>
2023-04-12 16:34:03 +0300

    Update PRIVMSG.cpp

--- src/commands/PRIVMSG.cpp
+++ src/commands/PRIVMSG.cpp
@@ -13,51 +13,55 @@ int Server::findUserByName(std::string name)
     return -1;
 }
 
-void    Server::broadcast(const std::vector<Client *> &clientList, std::string msg, std::string channelName, int excludeFd = -1)
+void    Server::broadcast(const std::vector<Client *> &clientList, std::string msg, int excludeFd)
 {
     for (size_t i = 0; i < clientList.size(); i++)
     {
         if (clientList[i]->getFd() == excludeFd)
             continue ;
-        ft_write(clientList[i]->getFd(), ":" + _clients[excludeFd]->getPrefixName() + " PRIVMSG " + channelName + " " + msg);
-
+        ft_write(clientList[i]->getFd(), msg);
     }
 }
 
 void    Server::privmsg(int fd, std::vector<std::string> token)
 {
-    //token[2][0] == '#' kanal mesajı | değilse -> bireysel mesajlaşma (kullanıcı kontrolü)
-    if (token[token.size() - 1][0] != ':' || token.size() != 3)
-    {
-        ft_write(fd, ":" + _clients[fd]->getPrefixName() + " 461 " + _clients[fd]->getNickName() + " PRIVMSG :Not enough parameters\r\n");
-        return ;
-    }
-    int nickFd = findUserByName(token[1]);
-    if (_clients.find(nickFd) != _clients.end())
-    {
-        std::string ping("PING");
-        std::string pong("PONG");
-        if (token[2].find_first_of(ping) != std::string::npos || token[2].find_first_of(pong) != std::string::npos)
-        {
-            std::string cmd = token[2].substr(2, 4);
-            size_t  pos = token[2].find_first_of(' ');
-            std::vector<std::string> newTok;
-            newTok.push_back(cmd);
-            newTok.push_back(token[2].substr(pos + 1));
-            if (_commands.find(newTok[0]) != _commands.end())
-                (this->*_commands[newTok[0]])(fd, newTok);
-            return ;
-        }
-        //FOUND USER
-        ft_write(_clients[nickFd]->getFd(), ":" + _clients[fd]->getPrefixName() + " PRIVMSG " + token[1] + " :" + token[2]);
-        return ;
-    }
-    else if (_channels.find(token[1]) != _channels.end())
-    {
-        //FOUND CHANNEL
-        broadcast(_channels.find(token[1])->second->_channelClients, token[2], token[1], fd);
-        return ;
-    }
-    //NO USER OR CHANNEL MATCHES
-    ft_write(fd, ":" + _clients[fd]->getPrefixName() + " 401 " + token[1] + ": No such Nick/Channel\r\n");
+	if (token.size() < 3) {
+		_clients[fd]->clientMsgSender(fd, ERR_NEEDMOREPARAMS(_clients[fd]->getNickName(), "PRIVMSG"));
+		return;
+	}
+
+	if (token[2][0] == ':')
+		token[2].erase(0, 1);
+
+	if (token[1][0] == '#') {
+		Channel *channel = _channels[token[1]];
+		if (!channel)
+		{
+			_clients[fd]->clientMsgSender(fd, ERR_NOSUCHCHANNEL(_clients[fd]->getNickName(), token[1]));
+			return;
+		}
+
+		if (channel->getClientAuthority())
+		{
+			size_t	i = 0;
+			for (i = 0 ; i < _channels[token[1]]->_channelClients.size() ; i++) // Kanala mesaj yollamak için şimdiki kullanıcının kanalda olması gereklidir.
+				if (_channels[token[1]]->_channelClients[i]->getNickName() == _clients[fd]->getNickName())
+					break;
+			if (i == _channels[token[1]]->_channelClients.size())
+			{
+				_clients[fd]->clientMsgSender(fd, ERR_CANNOTSENDTOCHAN(_clients[fd]->getNickName(), token[1]));
+				return;
+			}
+		}
+		broadcast(channel->_channelClients, RPL_PRIVMSG(_clients[fd]->getPrefixName(), token[1], token[2]), fd);
+		return;
+	}
+
+	int destFd = findUserByName(token[1]);
+	if (destFd == -1)
+	{
+		_clients[fd]->clientMsgSender(fd, ERR_NOSUCHNICK(_clients[fd]->getNickName(), token[1]));
+		return;
+	}
+	ft_write(destFd, RPL_PRIVMSG(_clients[fd]->getPrefixName(), token[1], token[2]));
 }
